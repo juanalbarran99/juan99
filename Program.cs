c85aff2e0da2a4e6105e0b44814670c262fa4707
juanalbarran99 <71295887+juanalbarran99@users.noreply.github.com>
2020-11-26 12:51:43 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figuras
+{
+    class Figura
+    {
+        public int Rx, Ry;
+        public Figura(int Rx, int Ry)
+        {
+            this.Rx = Rx;
+            this.Ry = Ry;
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!Hola Mundo!");
+            Console.Writeline("Soy p#to");
+            Console.ReadKey();
+        }
+    }
+}
+
